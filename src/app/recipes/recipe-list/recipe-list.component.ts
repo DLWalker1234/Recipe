6436b7c62299d8d4a75cb256fc8fb7c1567486fa
Dustin Walker <thewalkman1234@gmail.com>
2018-02-28 17:21:59 -0500

    recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is simply a test', 'https://www.seriouseats.com/recipes/images/2016/06/20160623-cuban-roast-pork-mojo-recipe-14-1500x1125.jpg')
+  ];
 
   constructor() { }
 
