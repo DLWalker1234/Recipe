7bc01485d1c54bd36a2a0d6b71e687acaea51414
Dustin Walker <thewalkman1234@gmail.com>
2018-03-16 12:59:43 -0400

    recipe item componenet

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe','This is simply a test', 'https://www.seriouseats.com/recipes/images/2016/06/20160623-cuban-roast-pork-mojo-recipe-14-1500x1125.jpg')
   ];
@@ -14,6 +15,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+
   }
 
 }
