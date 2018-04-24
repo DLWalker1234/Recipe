d23daadd3799c60eb39901e4f35c45a16b86c13d
Dustin Walker <thewalkman1234@gmail.com>
2018-04-24 15:50:12 -0400

    sign up complete

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+export class AuthService {
+
+    signupUser(email: string, password: string) {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((resolve) => {
+                console.log('User Signed Up');
+            })
+            .catch(
+                error => {
+                    console.log(error)
+                }
+            )
+    }
+}
